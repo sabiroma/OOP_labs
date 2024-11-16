7d9378c686f11c762e58b82c524026cfb2cd5acf
sabiroma <srd-05@mail.ru>
2024-11-16 13:45:09 +0300

    Add files via upload

---
+++ lab08/lab08_javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example","c":"lab08","l":"Bus()"},{"p":"org.example","c":"lab08","l":"lab08()","u":"%3Cinit%3E()"},{"p":"org.example","c":"lab08","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
